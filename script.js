44ebcdb78e3b5c4e033ee8ee20040626f0ae5a45
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-01-30 21:39:20 +0700

    Add cat image

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+var wakeuptime = 7;
+var noon = 12;
+var lunchtime = 12;
+var naptime = lunchtime + 2;
+var partytime;
+var evening = 18;
+var event = document.getElementById("event-quote");
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var quote = "Good morning";
+  m = checkTime(m);
+  s = checkTime(s);
+  var median = "AM";
+  if(h >= noon){
+    median = "PM";
+    quote = "Good afternoon";
   }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if(h>= evening){
+    quote = "Good evening";
   }
+  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + " " + median + "!";
+  if(quote != event.innerHTML){
+    event.innerHTML = quote;
+  }
+  var t = setTimeout(startTime, 500);
+}  
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
 
 startTime();
